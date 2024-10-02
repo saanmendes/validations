ec2d78184d95d920ed44cd58d19aed4b86d654c1
sandra mendes <sunvhs@gmail.com>
2024-10-02 11:54:27 -0300

    Add validation in UserRegisterDTO

--- src/main/java/com/br/zup/validateions/controllers/dtos/UserRegisterDTO.java
+++ src/main/java/com/br/zup/validateions/controllers/dtos/UserRegisterDTO.java
@@ -1,9 +1,22 @@
 package com.br.zup.validateions.controllers.dtos;
 
 public class UserRegisterDTO {
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 2, message = "Deve conter nome e sobrenome")
     private String name;
+
+    @Min(value = 18, message = "Deve ser maior de idade")
+    @Max(value = 110, message = "Deve ser menor que 110 anos")
     private int age;
+
+    @Min(value = 1914, message = "Deve ter menos que 110 anos")
+    @Max(value = 2024, message = "Deve ter menos que 110 anos")
     private int yearOfBirth;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email deve estar em um formato válido")
+    @Pattern(regexp = "^[a-zA-Z0-9._]+@[a-zA-Z]+\\.[a-zA-Z]{2,}$",
+            message = "Email deve seguir o padrão válido (exemplo: usuario@dominio.com)")
     private String email;
 
     public UserRegisterDTO() {
